411499f6e7c875904f301501652b230fd87628dd
Scott Baker <scott@perturb.org>
2013-11-05 13:12:41 -0800

    Rename KRUMO_CAPTURE -> KRUMO_RETURN

--- class.krumo.php
+++ class.krumo.php
@@ -23,8 +23,8 @@ if (!defined('KRUMO_DIR')) {
     define('KRUMO_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 }
 
-if (!defined('KRUMO_CAPTURE')) {
-    define('KRUMO_CAPTURE','158bafa5-b505-4661-9904-46504e00a5bb');
+if (!defined('KRUMO_RETURN')) {
+    define('KRUMO_RETURN','158bafa5-b505-4661-9904-46504e00a5bb');
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -439,7 +439,7 @@ class krumo {
     */
     public static function dump($data, $capture = '') {
         // If we're capturing call dump() with just data and capture the output
-        if ($capture === KRUMO_CAPTURE) {
+        if ($capture === KRUMO_RETURN) {
             ob_start();
 
             krumo::dump($data);
