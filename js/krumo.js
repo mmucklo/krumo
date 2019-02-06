27eeaf753d0cb3af074fcafa593b2aed9357c195
Scott Baker <scott.baker@directlink.coop>
2019-02-06 13:58:26 -0800

    Make ctrl + click work with multiple levels of collapsed entries

--- js/krumo.js
+++ js/krumo.js
@@ -12,8 +12,7 @@
 /**
 * Krumo JS Class
 */
-function krumo() {
-	}
+function krumo() { }
 
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -27,8 +26,8 @@ function krumo() {
 krumo.reclass = function(el, className) {
 	if (el.className.indexOf(className) < 0) {
 		el.className += (' ' + className);
-		}
 	}
+}
 
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -42,8 +41,8 @@ krumo.reclass = function(el, className) {
 krumo.unclass = function(el, className) {
 	if (el.className.indexOf(className) > -1) {
 		el.className = el.className.replace(className, '');
-		}
 	}
+}
 
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -54,37 +53,26 @@ krumo.unclass = function(el, className) {
 * @return void
 */
 krumo.toggle = function(event) {
-	console.log(event);
-
-	var el   = event.target.parentNode;
-	var ctrl = event.ctrlKey;
+	var elem        = event.target.closest(".krumo-expand");
+	var ctrl        = event.ctrlKey;
+	var is_expanded = !elem.classList.contains("krumo-opened");
 
 	// Adding a control to you click does an expand/collapse all
 	if (ctrl) {
-		var is_expand = !el.classList.contains("krumo-opened");
-
-		if (is_expand) {
-			krumo.expand_all(el);
+		if (is_expanded) {
+			krumo.expand_all(elem);
 		} else {
-			krumo.collapse_all(el);
+			krumo.collapse_all(elem);
 		}
 	} else {
-		var ul = el.parentNode.getElementsByTagName('ul');
-		for (var i=0; i<ul.length; i++) {
-			if (ul[i].parentNode.parentNode == el.parentNode) {
-				ul[i].parentNode.style.display = (ul[i].parentNode.style.display == 'none')
-					? 'block'
-					: 'none';
-			}
-		}
-
-		// toggle class
-		if (ul[0].parentNode.style.display == 'block') {
-			krumo.reclass(el, 'krumo-opened');
+		if (is_expanded) {
+			krumo.expand(elem);
 		} else {
-			krumo.unclass(el, 'krumo-opened');
+			krumo.collapse(elem);
 		}
 	}
+
+	event.stopPropagation();
 }
 
 krumo.expand = function(el) {
@@ -94,6 +82,15 @@ krumo.expand = function(el) {
 
 	nest.style.display = 'block';
 	krumo.reclass(exp,"krumo-opened");
+}
+
+krumo.collapse = function(el) {
+	paren = el.parentNode;
+	nest  = paren.querySelector(".krumo-nest");
+	exp   = paren.querySelector(".krumo-expand");
+
+	nest.style.display = 'none';
+	krumo.unclass(exp,"krumo-opened");
 }
 
 krumo.expand_all = function(el) {
@@ -134,7 +131,7 @@ krumo.collapse_all = function(el) {
 */
 krumo.over = function(el) {
 	krumo.reclass(el, 'krumo-hover');
-	}
+}
 
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -147,89 +144,27 @@ krumo.over = function(el) {
 
 krumo.out = function(el) {
 	krumo.unclass(el, 'krumo-hover');
-	}
+}
 
 /////////////////////////////////////////////////////////////////////////////
 
-// Get element by ID
-krumo.get_id = function(str) {
-	var ret = document.getElementById(str);
+function qsa(search) {
+	var elems = document.querySelectorAll(search);
 
-	return ret;
-}
-
-// Get element by Class
-krumo.get_class = function(str) {
-	var elems = document.getElementsByClassName(str);
-	var ret = new Array();
-
-	// Just get the objects (not the extra stuff)
-	for (var i in elems) {
-		var elem = elems[i];
-		if (typeof(elem) === 'object') {
-			ret.push(elem);
-		}
-	}
-
-	return ret;
-}
-
-// This is a poor mans querySelectorAll().
-// querySelectorAll() isn't supported 100% until
-// IE9, so we have to use this work around until
-// we can stop supporting IE8
-krumo.find = function(str) {
-	if (!str) { return false; }
-
-	var first  = str.substr(0,1);
-	var remain = str.substr(1);
-
-	if (first === ".") {
-		return krumo.get_class(remain);
-	} else if (first === "#") {
-		return krumo.get_id(remain);
-	} else {
-		return false;
-	}
-}
-
-function toggle_expand_all() {
-	// Find all the expandable items
-	var elems = krumo.find('.krumo-expand');
-	if (elems.length === 0) { return false; }
-
-	// Find the first expandable element and see what state it is in currently
-	var action = 'expand';
-	if (elems[0].nextSibling.style.display === 'block' || elems[0].nextSibling.style.display === '') {
-		action = 'collapse';
-	}
-
-	// Expand each item
-	for (var i in elems) {
-		var item = elems[i];
-
-		// The sibling is the hidden object
-		var sib = item.nextSibling;
-
-		if (action === 'expand') {
-			sib.style.display = 'block';
-			// Give the clicked item the krumo-opened class
-			krumo.reclass(item, 'krumo-opened');
-		} else {
-			sib.style.display = 'none';
-			// Remove the krumo-opened class
-			krumo.unclass(item, 'krumo-opened');
-		}
-	}
+	return elems;
 }
 
 function init_click() {
-	var elems = krumo.find('.krumo-expand');
+	var elems = qsa('.krumo-expand');
 
 	// Add a click item to everything that's expandable
 	elems.forEach(function(el) {
-		el.addEventListener("click", krumo.toggle);
+		el.parentNode.addEventListener("click", krumo.toggle);
 	});
 }
 
-init_click();
+// Equivalent to $(document).ready() in JQUery
+// https://stackoverflow.com/questions/2304941/what-is-the-non-jquery-equivalent-of-document-ready
+document.addEventListener("DOMContentLoaded", function() {
+	init_click();
+});
