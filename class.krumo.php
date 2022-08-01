c2972cc256e76197a156dfccb18affa439364585
Dennis Riehle <webmaster@riehle-web.com>
2022-08-01 16:48:18 +0200

    Fix deprecated message appearing in web context with PHP 8.1 (#64)
    * fix deprecated message appearing in web context with PHP 8.1
    
    * use to PHP7-syntax

--- class.krumo.php
+++ class.krumo.php
@@ -499,8 +499,9 @@ class Krumo
         $_ = debug_backtrace();
         while ($d = array_pop($_)) {
             $callback = static::$lineNumberTestCallback;
-            $function = strToLower($d['function']);
-            if (in_array($function, array("krumo","k","kd")) || (strToLower(@$d['class']) == 'krumo') || (is_callable($callback) && call_user_func($callback, $d))) {
+            $class    = strtolower($d['class'] ?? '');
+            $function = strtolower($d['function'] ?? '');
+            if (in_array($function, array('krumo','k','kd')) || $class == 'krumo' || (is_callable($callback) && call_user_func($callback, $d))) {
                 break;
             }
         }
