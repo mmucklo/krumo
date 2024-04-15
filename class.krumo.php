04e2ad2600ccfc5ee688670587e0ff8778986fe2
Scott Baker <scott@perturb.org>
2024-04-14 17:36:51 -0700

    PHP 8.2 fix for floating point number conversion warnings (#69)

--- class.krumo.php
+++ class.krumo.php
@@ -1425,8 +1425,9 @@ class Krumo
         }
     }
 
-    private static function is_datetime($name, $value)
-    {
+    private static function is_datetime($name, $value) {
+        $value = intval($value);
+
         // If the name contains date or time, and the value looks like a unixtime
         if (preg_match("/date|time/i", $name) && (is_numeric($value) && $value > 10000000 && $value < 4000000000)) {
             $ret = date("r", $value);
